ae9eaf40740fa81a6c3be1b5ef799d1dc50f22a6
huangwanzi <man.tanawan2546@gmail.com>
2023-12-15 04:49:41 +0700

    Change label to Thai language in IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,12 +4,27 @@
       <q-input
         filled
         v-model="name"
+        label="ชื่อ - สกุล"
+        hint="ชื่อและสกุล"
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || 'กรุณาพิมพ์ชื่อ']"
+      />
+      <q-input
+        filled
+        v-model="name"
         label="Your name *"
         hint="Name and surname"
         lazy-rules
         :rules="[(val) => (val && val.length > 0) || 'Please type something']"
       />
-
+      <q-input
+        filled
+        v-model="name"
+        label="Your name *"
+        hint="Name and surname"
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+      />
       <q-input
         filled
         type="number"
